e3e64d07b279f6d61aea5bc03a451613640754e2
Tsi777 <tsionphilipos6@gmail.com>
2025-09-29 15:59:03 +0300

    Create views.py

---
+++ properties/views.py
@@ -0,0 +1,19 @@
+from django.views.decorators.cache import cache_page
+from django.http import JsonResponse
+from .models import Property
+from .utils import get_all_properties
+
+@cache_page(60 * 15)  # 15 minutes
+def property_list(request):
+    properties = get_all_properties()
+    data = [
+        {
+            "id": p.id,
+            "title": p.title,
+            "description": p.description,
+            "price": str(p.price),
+            "location": p.location,
+            "created_at": p.created_at.isoformat()
+        } for p in properties
+    ]
+    return JsonResponse({"properties": data}, safe=False)
